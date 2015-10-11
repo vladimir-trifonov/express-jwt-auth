45a48823b79df142435f9394c2d3308d5c6977f8
Vladimir Trifonov <vladimir.trifonov@clevertech.biz>
2015-10-11 15:06:30 +0300

    HTTPS activated

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@
 
 var debug = require('debug')('app:' + process.pid),
 	env = process.env.NODE_ENV || 'development',
-	//path = require('path'),
-	//fs = require('fs'),
+	path = require('path'),
+	fs = require('fs'),
 	express = require('express'),
 	app = express();
 
@@ -28,15 +28,15 @@ require('http').createServer(app).listen(config.http_port, function() {
 	debug('HTTP Server listening on port: %s, in %s mode', config.http_port, app.get('env'));
 });
 
-// debug('Creating HTTPS server on port: %s', config.https_port);
-// require('https').createServer({
-// 	key: fs.readFileSync(path.join(__dirname, 'keys', 'server.key')),
-// 	cert: fs.readFileSync(path.join(__dirname, 'keys', 'server.crt')),
-// 	ca: fs.readFileSync(path.join(__dirname, 'keys', 'ca.crt')),
-// 	requestCert: true,
-// 	rejectUnauthorized: false
-// }, app).listen(config.https_port, function() {
-// 	debug('HTTPS Server listening on port: %s, in %s mode', config.https_port, app.get('env'));
-// });
+debug('Creating HTTPS server on port: %s', config.https_port);
+require('https').createServer({
+	key: fs.readFileSync(path.join(__dirname, 'keys', 'server.key')),
+	cert: fs.readFileSync(path.join(__dirname, 'keys', 'server.crt')),
+	ca: fs.readFileSync(path.join(__dirname, 'keys', 'ca.crt')),
+	requestCert: true,
+	rejectUnauthorized: false
+}, app).listen(config.https_port, function() {
+	debug('HTTPS Server listening on port: %s, in %s mode', config.https_port, app.get('env'));
+});
 
 exports.app = app;
